521b6ac2c9facde129efc4a6c053e5f64ec9a850
Steve Womack <swomack@pivotal.io>
2019-08-14 14:54:18 -0500

    Updates to Spring libs

--- session_06/lab_06/hello-world/hello-ui/src/main/java/io/pivotal/helloui/HelloUiApplication.java
+++ session_06/lab_06/hello-world/hello-ui/src/main/java/io/pivotal/helloui/HelloUiApplication.java
@@ -4,13 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableCircuitBreaker
+@Configuration
 public class HelloUiApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(HelloUiApplication.class, args);
     }
 }
--- session_06/lab_06/hello-world/hello-ui/src/main/java/io/pivotal/helloui/controller/SayHelloCommand.java
+++ session_06/lab_06/hello-world/hello-ui/src/main/java/io/pivotal/helloui/controller/SayHelloCommand.java
@@ -1,20 +1,27 @@
 package io.pivotal.helloui.controller;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import io.pivotal.helloui.domain.HelloResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
 @Component
+@Configuration
 public class SayHelloCommand {
+
     @Autowired
-    @LoadBalanced
     RestTemplate restTemplate;
 
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        // Do any additional configuration here
+        return builder.build();
+    }
 
     @HystrixCommand(fallbackMethod = "sayHelloFallback")
     public String sayHello(String toWho) {
