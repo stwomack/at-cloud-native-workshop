99f5539217d2bebb3ebe7e08cff45e2db813665f
Steve Womack <swomack@pivotal.io>
2019-08-14 15:55:36 -0500

    Turns off spring security

--- session_06/lab_06/hello-world/hello-server/src/main/java/io/pivotal/HelloServerApplication.java
+++ session_06/lab_06/hello-world/hello-server/src/main/java/io/pivotal/HelloServerApplication.java
@@ -2,6 +2,7 @@ package io.pivotal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
---
+++ session_06/lab_06/hello-world/hello-ui/src/main/java/io/pivotal/helloui/ApplicationSecurity.java
@@ -0,0 +1,14 @@
+package io.pivotal.helloui;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/**");
+    }
+}
