769352cf238e7fa5d3a9545b517890dfc1a809c9
Guillermo Tantachuco <gtantachuco@gmail.com>
2019-10-02 00:25:53 -0500

    custom metrics

--- session_05/lab_05/initial/hello-spring-boot/src/main/java/io/pivotal/spring/hello/GreetingService.java
+++ session_05/lab_05/initial/hello-spring-boot/src/main/java/io/pivotal/spring/hello/GreetingService.java
@@ -1,10 +1,8 @@
 package io.pivotal.spring.hello;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
-//import org.springframework.boot.actuate.metrics.CounterService;
 import org.springframework.stereotype.Component;
 
 import io.micrometer.core.instrument.Counter;
